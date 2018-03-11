06639988601e499fb1550492981c64957cb50f8c
Thái Minh Sang <sang024854@gmail.com>
2018-03-11 14:16:07 +0700

    Add check source img when create

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -45,8 +45,6 @@ $(document).ready(function () {
 				var newContent = '<div class="col-md-4 card-loop" card-id="' + data._id + '">\n' +
 						'\t\t\t\t\t\t<div class="card mb-4 box-shadow">\n' +
 						'\t\t\t\t\t\t\t<img class="card-img-top"\n' +
-						'\t\t\t\t\t\t\t\t src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22348%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20348%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16210a7ae5c%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A17pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16210a7ae5c%22%3E%3Crect%20width%3D%22348%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.7265625%22%20y%3D%22120.3%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"\n' +
-						'\t\t\t\t\t\t\t\t data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"\n' +
 						'\t\t\t\t\t\t\t\t alt="Card image cap" height="225" width="348">\n' +
 						'\t\t\t\t\t\t\t<div class="card-body">\n' +
 						'\t\t\t\t\t\t\t\t<h5 class="card-title">' + data.title + '</h5>\n' +
@@ -68,6 +66,8 @@ $(document).ready(function () {
 				alert(JSON.stringify("Create Success!"));
 				$("#createModal").modal('toggle');
 				$(".row").append(newContent);
+				var card = $('.card-loop[card-id="' + data._id + '"]');
+				validateImageURL(card, data.images);
 			},
 			fail: function (data) {
 				alert(JSON.stringify(data.message));
