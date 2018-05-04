d484a22d5d17a9965c136764940129ba8b7108bf
Thái Minh Sang <sang024854@gmail.com>
2018-05-05 00:52:21 +0700

    add cors allow all method

--- app.js
+++ app.js
@@ -55,6 +55,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "*");
 	next();
 });
 
